9bcc3ff18dde550f8cfae85485a2b0c5139c0ee1
Gabriela Fanganiello <gabrielafanganiello@gmail.com>
2023-04-19 16:05:28 -0600

    faz jogada dnv

--- Funções/Geral.py
+++ Funções/Geral.py
@@ -21,6 +21,13 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
         frota[nome_navio] = [posicao]
     return frota
 
+#Exercicio faz jogada 
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    elif tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    return tabuleiro
 
 #Exercicio Posiciona Frota
 def posiciona_frota(informacoes_navios):
