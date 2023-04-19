f47d78ec5d374ef073bc02a5c832029c2c67a73b
LuanaPLGuimaraes <luanaplg@a.insper.edu.br>
2023-04-19 16:04:31 -0300

    tentando commitar junto

--- Funções/Geral.py
+++ Funções/Geral.py
@@ -21,10 +21,7 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
         frota[nome_navio] = [posicao]
     return frota
 
-#Exercicio faz jogada 
-def faz_jogada(tabuleiro, linha, coluna):
-    if tabuleiro[linha][coluna] == 0:
-        tabuleiro[linha][coluna] = '-'
-    elif tabuleiro[linha][coluna] == 1:
-        tabuleiro[linha][coluna] = 'X'
-    return tabuleiro
+
+#Exercicio Posiciona Frota
+def posiciona_frota(informacoes_navios):
+    posicionados = preenche_frota(informacoes_navios)
