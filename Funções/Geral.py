a847f60dd28443fdac4c49093cf8e68e839c08fd
LuanaPLGuimaraes <luanaplg@a.insper.edu.br>
2023-04-19 13:40:50 -0300

    funao preenche_frota

--- Funções/Geral.py
+++ Funções/Geral.py
@@ -8,3 +8,21 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             posicao.append([linha, coluna])
             coluna+=1
     return posicao
+
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicao=[]
+    for i in range(tamanho):
+        if orientacao=='vertical':
+            posicao.append([linha, coluna])
+            linha+=1
+        elif orientacao=='horizontal':
+            posicao.append([linha, coluna])
+            coluna+=1
+
+    if nome_navio in frota.keys():
+        frota[nome_navio].append(posicao)
+
+    else:
+        frota[nome_navio] = [posicao]
+    return frota
