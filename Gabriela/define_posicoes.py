571565ebfc1b3e862bdc56ba99636cc59fdb29f9
Gabriela Fanganiello <gabrielafanganiello@gmail.com>
2023-04-19 12:31:37 -0600

    define posicoes

--- Gabriela/define_posicoes.py
+++ Gabriela/define_posicoes.py
@@ -1,1 +1,10 @@
-print('ex define posicoes')
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicao=[]
+    for i in range(tamanho):
+        if orientacao=='vertical':
+            posicao.append([linha, coluna])
+            linha+=1
+        elif orientacao=='horizontal':
+            posicao.append([linha, coluna])
+            coluna+=1
+    print(posicao)
