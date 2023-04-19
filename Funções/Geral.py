cb4b47be262d6e368ff481336b7a1f67b357b6cf
Gabriela Fanganiello <gabrielafanganiello@gmail.com>
2023-04-19 13:16:12 -0600

    ex define funcoes

---
+++ Funções/Geral.py
@@ -0,0 +1,10 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicao=[]
+    for i in range(tamanho):
+        if orientacao=='vertical':
+            posicao.append([linha, coluna])
+            linha+=1
+        elif orientacao=='horizontal':
+            posicao.append([linha, coluna])
+            coluna+=1
+    return posicao
